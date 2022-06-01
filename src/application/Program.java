04f7d6e36a66c53192b5f16a465edc7466dd456b
Roner <ronerumb@gmail.com>
2022-06-01 20:56:08 -0300

    java project

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1,"Books");
+		System.out.println(obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+
+	}
+
+}
